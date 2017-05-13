433b7ec4183162fa69997a4767950ab2fa9ee976
Danie Schwartz <schwartz.napoleon@gmail.com>
2017-05-13 23:24:44 +0200

    Update User Write to database success

--- ITRW211Projek2017/UpdateUser.cs
+++ ITRW211Projek2017/UpdateUser.cs
@@ -50,7 +50,29 @@ namespace ITRW211Projek2017
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
+            bool isAdmin = false;
+            if (rdbAdminYes.Checked)
+            {
+                isAdmin = true;
+            }
+            if (txtName.Text != "" && txtSurname.Text != "")
+            {
+                oleDbConnection.Open();
+                var myCommand = new OleDbCommand();
+                myCommand.Connection = oleDbConnection;
+                myCommand.CommandText = "Update Users set Employee_Name = '"+txtName.Text+"',Employee_Surname ='"+txtSurname.Text+"',Employee_Id = '"+txtUsername.Text
+                    +"',Admin_rights = "+isAdmin+" WHERE ID ="+userID+"";
+                //myCommand.CommandText =
+                //    "INSERT INTO Users (Employee_Name,Employee_Surname,Employee_Id,Admin_rights) Values('" +
+                //    Naam + "','" + Surname + "','" + userName + "'," + rdbAdmin.Checked + ")";
+                myCommand.ExecuteNonQuery();
+                MessageBox.Show("Data inserted successfully");
+                oleDbConnection.Close();
 
+                Hide();
+                var form = new AddandRemoveUser();
+                form.Show();
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
