30a3ba33ce6996d721bff072c22137d079aeacb8
Danie Schwartz <schwartz.napoleon@gmail.com>
2017-05-14 11:35:41 +0200

    Klaar laaste een

--- ITRW211Projek2017/StockReport.Designer.cs
+++ ITRW211Projek2017/StockReport.Designer.cs
@@ -38,13 +38,13 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(584, 279);
+            this.dataGridView1.Size = new System.Drawing.Size(548, 279);
             this.dataGridView1.TabIndex = 0;
             // 
             // btnClose
             // 
             this.btnClose.BackgroundImage = global::ITRW211Projek2017.Properties.Resources.images__1_;
-            this.btnClose.Location = new System.Drawing.Point(511, 297);
+            this.btnClose.Location = new System.Drawing.Point(476, 297);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(84, 30);
             this.btnClose.TabIndex = 1;
@@ -58,7 +58,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ITRW211Projek2017.Properties.Resources.Capture;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(607, 335);
+            this.ClientSize = new System.Drawing.Size(575, 335);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.dataGridView1);
             this.DoubleBuffered = true;
